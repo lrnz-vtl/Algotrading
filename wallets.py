c998cb1ff3cd0010949103a54c266051b3d1fd88
ciubecca <lorenzovitale89@gmail.com>
2021-11-10 23:21:57 +0000

    added failing tests

--- wallets.py
+++ wallets.py
@@ -4,28 +4,39 @@ import numpy as np
 from typing import Dict, Iterable, Tuple
 from tinydata import TinyData
 
+
 def get_asset_data(asset_id):
     asset = requests.get(url=f'https://algoexplorerapi.io/idx2/v2/assets/{asset_id}').json()
     return asset['asset']
 
-def get_account_data(address=None):
+
+def get_account_data(address=None, testnet=False):
     """Query wallet data from AlgoExplorer"""
 
+    if testnet:
+        url = f'https://algoexplorerapi.io/v2/accounts/{address}'
+    else:
+        url = f'https://testnet.algoexplorerapi.io/api-dev/v2/accounts/{address}'
+
     # as specified here https://algoexplorer.io/api-dev/v2
-    data = requests.get(url=f'https://algoexplorerapi.io/v2/accounts/{address}').json()
+    data = requests.get(url=url).json()
 
     # set up dictionary with values for each coin
-    coins = {0 : data['amount']/10**6}
+    coins = {0: data['amount'] / 10 ** 6}
     for d in data['assets']:
-        coins[d['asset-id']] = d['amount']/10**get_asset_data(d['asset-id'])['params']['decimals']
-        
+        coins[d['asset-id']] = d['amount'] / 10 ** get_asset_data(d['asset-id'])['params']['decimals']
+
     # return the assets in the wallet
     # note: we are discarding some info here (rewards, offline, frozen, etc)
     return coins
 
+
 class Portfolio:
-    def __init__(self, address=None):
+    def __init__(self, address=None, testnet=False):
         """Set up the portfolio from a wallet address"""
+
+        self.testnet = testnet
+
         if not address:
             # set up a dummy portfolio
             self.coins: Dict[int, int] = {0: 100}
@@ -34,7 +45,7 @@ class Portfolio:
             self.update()
 
     def update(self):
-        self.coins: Dict[int, int] = get_account_data(self.address)
+        self.coins: Dict[int, int] = get_account_data(self.address, self.testnet)
 
     def __getitem__(self, asset_id):
         return self.coins[asset_id]
