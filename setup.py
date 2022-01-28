9f9101d7909d57e1f67c02aeb332061f65c18a9f
lorenzo <lorenzovitale89@gmail.com>
2022-01-28 15:57:20 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+import setuptools
+
+setuptools.setup(name='algo',
+                 version='0.1.0',
+                 packages=setuptools.find_packages(include=["algo", "algo.*"])
+                 )
