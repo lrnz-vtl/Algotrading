9e243c921bed670e60d4f599f0fbd392da9ce577
lorenzo <lorenzovitale89@gmail.com>
2022-02-06 20:08:47 +0100

    plotting prices

--- scripts/make_price_cache.py
+++ scripts/make_price_cache.py
@@ -1,17 +1,22 @@
 import datetime
 from algo.blockchain.process_prices import PriceCacher
 from tinyman.v1.client import TinymanMainnetClient
+import argparse
 
-universe_cache_name = '20220202-110531'
+if __name__ == '__main__':
 
-cache_name = '20220206'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', dest='universe_cache_name', type=str, required=True)
+    parser.add_argument('-c', dest='cache_name', type=str, required=True)
 
-date_min = datetime.datetime(year=2022, month=1, day=20)
+    args = parser.parse_args()
 
-pc = PriceCacher(client=TinymanMainnetClient(),
-                 universe_cache_name=universe_cache_name,
-                 date_min=date_min,
-                 date_max=None
-                 )
-pc.cache(cache_name)
+    date_min = datetime.datetime(year=2022, month=1, day=20)
+
+    pc = PriceCacher(client=TinymanMainnetClient(),
+                     universe_cache_name=args.universe_cache_name,
+                     date_min=date_min,
+                     date_max=None
+                     )
+    pc.cache(args.cache_name)
 
