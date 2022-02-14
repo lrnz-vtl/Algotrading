49d65c2a9b3b3c8785650ccaa26f67a496e1ea46
fdreyer <frederic.dreyer@gmail.com>
2022-02-14 00:28:07 +0000

    started debugging volume stream, but still broken

--- algo/blockchain/stream.py
+++ algo/blockchain/stream.py
@@ -1,4 +1,4 @@
-from algo.blockchain.process_volumes import PoolTransaction
+from algo.blockchain.process_volumes import PoolTransaction, Swap, is_fee_payment
 from algo.blockchain.process_prices import PoolState, get_pool_state_txn
 from algo.universe.universe import SmallUniverse
 from typing import Optional
@@ -54,7 +54,19 @@ class PriceVolumeStream:
         self.pools = {x.address : (x.asset1_id, x.asset2_id) for x in SmallUniverse.from_cache(cache_file).pools}
         self.prices = {pool : list() for pool in self.pools.keys()}
         self.volumes = {pool : list() for pool in self.pools.keys()}
+        self.transaction_out = {pool : None for pool in self.pools.keys()}
+        self.transaction_in = {pool : None for pool in self.pools.keys()}
+        
     def scrape(self):
+        
+        def is_transaction_in(tx: PoolTransaction, transaction_out: PoolTransaction,
+                              asset1_id: int, asset2_id: int):
+            return tx.counterparty == transaction_out.counterparty \
+                   and tx.asset_id != transaction_out.asset_id \
+                   and tx.asset_id in [asset1_id, asset2_id] \
+                   and not is_fee_payment(tx)
+
+        
         for pool, tx in self.dataStream.next_transaction():
             pt = None
             if tx['tx-type']=='appl':
@@ -68,4 +80,37 @@ class PriceVolumeStream:
                 key='asset-transfer-transaction'
                 pt = get_pool_transaction_txn(tx, pool, key, tx[key]['asset-id'])
             if pt:
-                self.volumes[pool].append(pt)
+                asset1_id = self.pools[pool][0]
+                asset2_id = self.pools[pool][1]
+                # adapted from algo.blockchain.process_volumes.SwapScraper.scrape
+                if self.transaction_out[pool]:
+                    if self.transaction_in[pool]:
+                        if is_fee_payment(pt) and pt.counterparty == self.transaction_in[pool].counterparty:
+                            if self.transaction_in[pool].asset_id == asset1_id \
+                               and self.transaction_out[pool].asset_id == asset2_id:
+                                asset1_amount = self.transaction_in[pool].amount
+                                asset2_amount = self.transaction_out[pool].amount
+                            elif self.transaction_in[pool].asset_id == asset2_id \
+                                 and self.transaction_out[pool].asset_id == asset1_id:
+                                asset2_amount = self.transaction_in[pool].amount
+                                asset1_amount = self.transaction_out[pool].amount
+                            else:
+                                raise ValueError
+                            assert self.transaction_in[pool].amount > 0 \
+                                and self.transaction_out[pool].amount < 0
+                            swap = Swap(asset1_amount=asset1_amount,
+                                        asset2_amount=asset2_amount,
+                                        counterparty=pt.counterparty,
+                                        block=pt.block,
+                                        time=pt.time)
+                            self.volumes[pool].append(swap)
+                        self.transaction_out[pool] = None
+                        self.transaction_in[pool] = None
+                    else:
+                        if is_transaction_in(pt, self.transaction_out[pool], asset1_id, asset2_id):
+                            self.transaction_in[pool] = pt
+                        else:
+                            self.transaction_out[pool] = None
+                else:
+                    if pt.amount < 0 and pt.asset_id in [asset1_id, asset2_id]:
+                        self.transaction_out[pool] = pt
