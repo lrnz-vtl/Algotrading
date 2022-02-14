24bc17461b07d5f341d21631c99d9d94e0505a40
lorenzo <lorenzovitale89@gmail.com>
2022-02-14 19:57:06 +0100

    fixed risk optimisation

--- algo/trading/optimizer.py
+++ algo/trading/optimizer.py
@@ -3,8 +3,11 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Optional
 
+logger = logging.getLogger(__name__)
+
 # TODO Check me
 FIXED_FEE_ALGOS = 0.003
+FIXED_FEE_MUALGOS = FIXED_FEE_ALGOS * 10 ** 6
 
 FEE_BPS = (1000 / 997 - 1.0)
 
@@ -18,6 +21,29 @@ class OptimizedBuy:
     max_profitable_slippage_bps: float
 
 
+@dataclass
+class TradeCostsMualgo:
+    quadratic_impact_cost_mulago: float
+    linear_impact_cost_mulago: float
+
+
+@dataclass
+class TradeCostsOther:
+    quadratic_impact_cost_other: float
+    linear_impact_cost_other: float
+
+    def to_mualgo_basis(self, asset_price: float) -> TradeCostsMualgo:
+        return TradeCostsMualgo(self.quadratic_impact_cost_other * asset_price,
+                                self.linear_impact_cost_other * asset_price
+                                )
+
+
+@dataclass
+class OptimizedBuyInfo:
+    optimized_buy: OptimizedBuy
+    trade_costs_other: TradeCostsOther
+
+
 class AssetType(Enum):
     ALGO = 1
     OTHER = 2
@@ -29,19 +55,29 @@ class OptimalSwap:
     optimised_buy: OptimizedBuy
 
 
+@dataclass
+class OptimalSwapInfo:
+    optimal_swap: OptimalSwap
+    trade_costs_mualgo: TradeCostsMualgo
+
+
 def optimal_amount_buy_asset(signal_bps: float,
                              impact_bps: float,
                              current_asset_reserves: int,
                              current_other_asset_reserves: int,
-                             risk_bps: float,
-                             fixed_fee_other: float) -> Optional[OptimizedBuy]:
+                             quadratic_risk_penalty: float,
+                             linear_risk_penalty: float,
+                             fixed_fee_other: float) -> Optional[OptimizedBuyInfo]:
     # Do not let impact trade us more
     if impact_bps < 0:
         impact_bps = 0
 
+    if impact_bps > 0:
+        pass
+
     asset_price = current_other_asset_reserves / current_asset_reserves
 
-    f_bps = signal_bps - impact_bps - FEE_BPS - EXPECTED_SLIPPAGE_BPS + risk_bps
+    f_bps = signal_bps - impact_bps - FEE_BPS - EXPECTED_SLIPPAGE_BPS - linear_risk_penalty
 
     if f_bps < 0:
         return None
@@ -50,22 +86,34 @@ def optimal_amount_buy_asset(signal_bps: float,
     avg_impact_cost_coef = (1 + FEE_BPS) / current_asset_reserves
 
     # Expected profit in the reference currency
-    # <Profit> = Amount * Price (f_bps - avg_impact_cost_coef * Amount) - fixed_fee_other
+    # <Profit> = Amount * Price (f_bps - avg_impact_cost_coef * Amount) - quadratic_risk_penalty * Amount^2 - fixed_fee_other
 
-    amount_profit_argmax = int(f_bps / (2 * avg_impact_cost_coef))
+    # TODO FIXME
+    amount_profit_argmax = int(f_bps / (2 * (avg_impact_cost_coef + asset_price*quadratic_risk_penalty)))
 
     max_profit_other = (amount_profit_argmax * asset_price * f_bps / 2.0) - fixed_fee_other
     if max_profit_other <= 0:
         return None
 
+    quadratic_impact_cost_other = (amount_profit_argmax ** 2) * asset_price * avg_impact_cost_coef
+    linear_impact_cost_other = amount_profit_argmax * asset_price * impact_bps
+
     max_additional_slippage = f_bps - 2 * fixed_fee_other / (amount_profit_argmax * asset_price)
-    return OptimizedBuy(amount_profit_argmax, max_additional_slippage)
+    return OptimizedBuyInfo(
+        optimized_buy=OptimizedBuy(
+            amount=amount_profit_argmax,
+            max_profitable_slippage_bps=max_additional_slippage),
+        trade_costs_other=TradeCostsOther(
+            quadratic_impact_cost_other=quadratic_impact_cost_other,
+            linear_impact_cost_other=linear_impact_cost_other
+        )
+    )
 
 
 class Optimizer:
-    def __init__(self, asset1: int, risk_bps_per_algo: float):
+    def __init__(self, asset1: int, risk_coef: float):
         self.asset1 = asset1
-        self.risk_bps_per_mualgo = risk_bps_per_algo / 10**6
+        self.risk_coef = risk_coef
         assert self.asset1 > 0
 
         self.logger = logging.getLogger(__name__)
@@ -75,37 +123,42 @@ class Optimizer:
                             current_asa_position: int,
                             current_asa_reserves: int,
                             current_mualgo_reserves: int
-                            ) -> Optional[OptimalSwap]:
+                            ) -> Optional[OptimalSwapInfo]:
 
         asa_price_mualgo = current_mualgo_reserves / current_asa_reserves
 
-        optimized_asa_buy = optimal_amount_buy_asset(signal_bps=signal_bps,
-                                                     impact_bps=impact_bps,
-                                                     current_asset_reserves=current_asa_reserves,
-                                                     current_other_asset_reserves=current_mualgo_reserves,
-                                                     risk_bps=-current_asa_position * asa_price_mualgo * self.risk_bps_per_mualgo,
-                                                     # Upper bound pessimistic estimate for the fixed cost: if we buy now we have to exit later, so pay it twice
-                                                     fixed_fee_other=FIXED_FEE_ALGOS * 2
-                                                     )
+        quadratic_risk_penalty = self.risk_coef * asa_price_mualgo**2
+        linear_risk_penalty = 2.0 * self.risk_coef * current_asa_position * asa_price_mualgo**2
 
-        optimized_algo_buy = optimal_amount_buy_asset(signal_bps=1 / (1 + signal_bps) - 1.0,
-                                                      impact_bps=1 / (1 + impact_bps) - 1.0,
-                                                      current_asset_reserves=current_mualgo_reserves,
-                                                      current_other_asset_reserves=current_asa_reserves,
-                                                      risk_bps=+current_asa_position * asa_price_mualgo * self.risk_bps_per_mualgo,
-                                                      # TODO Check me
-                                                      fixed_fee_other=FIXED_FEE_ALGOS
-                                                      )
+        optimized_asa_buy_info = optimal_amount_buy_asset(signal_bps=signal_bps,
+                                                          impact_bps=impact_bps,
+                                                          current_asset_reserves=current_asa_reserves,
+                                                          current_other_asset_reserves=current_mualgo_reserves,
+                                                          quadratic_risk_penalty = quadratic_risk_penalty,
+                                                          linear_risk_penalty=linear_risk_penalty,
+                                                          # Upper bound pessimistic estimate for the fixed cost: if we buy now we have to exit later, so pay it twice
+                                                          fixed_fee_other=FIXED_FEE_MUALGOS * 2
+                                                          )
 
-        assert optimized_asa_buy is None or optimized_asa_buy is None
+        quadratic_risk_penalty = self.risk_coef
+        linear_risk_penalty = - 2.0 * self.risk_coef * current_asa_position * asa_price_mualgo
 
-        if optimized_asa_buy is not None:
-            return OptimalSwap(AssetType.OTHER, optimized_asa_buy)
-        elif optimized_algo_buy is not None:
-            self.logger.debug(f'Selling ASA and buying {optimized_algo_buy.amount / 10**6} algos')
-            return OptimalSwap(AssetType.ALGO, optimized_algo_buy)
+        optimized_algo_buy_info = optimal_amount_buy_asset(signal_bps=1 / (1 + signal_bps) - 1.0,
+                                                           impact_bps=1 / (1 + impact_bps) - 1.0,
+                                                           current_asset_reserves=current_mualgo_reserves,
+                                                           current_other_asset_reserves=current_asa_reserves,
+                                                           quadratic_risk_penalty=quadratic_risk_penalty,
+                                                           linear_risk_penalty=linear_risk_penalty,
+                                                           fixed_fee_other=FIXED_FEE_MUALGOS / asa_price_mualgo
+                                                           )
+
+        assert optimized_asa_buy_info is None or optimized_algo_buy_info is None
+
+        if optimized_asa_buy_info is not None:
+            return OptimalSwapInfo(OptimalSwap(AssetType.OTHER, optimized_asa_buy_info.optimized_buy),
+                                   optimized_asa_buy_info.trade_costs_other.to_mualgo_basis(1.0))
+        elif optimized_algo_buy_info is not None:
+            return OptimalSwapInfo(OptimalSwap(AssetType.ALGO, optimized_algo_buy_info.optimized_buy),
+                                   optimized_algo_buy_info.trade_costs_other.to_mualgo_basis(asa_price_mualgo))
         else:
             return None
-
-
-
--- algo/trading/simulator.py
+++ algo/trading/simulator.py
@@ -2,21 +2,21 @@ import datetime
 import unittest
 import logging
 from .impact import ASAImpactState, AlgoPoolSwap
-from .optimizer import Optimizer, OptimalSwap, OptimizedBuy, AssetType, FIXED_FEE_ALGOS
+from .optimizer import Optimizer, AssetType, FIXED_FEE_ALGOS
 from matplotlib import pyplot as plt
-
+import numpy as np
 
 class TestOptimizer(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
 
-        logging.basicConfig(level=logging.ERROR)
+        logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
 
         super().__init__(*args, **kwargs)
 
     def test_optimiser(self):
-        risk_bps_per_algo = 0.0001
+        risk_coef = 0.0000000001
 
         asa_asset_id = 470842789
         asa_reserves = 76845765066350
@@ -24,15 +24,15 @@ class TestOptimizer(unittest.TestCase):
 
         price = mualgo_reserves / asa_reserves
 
-        algo_position = 0
+        mualgo_position = 0
         asa_position = asa_reserves // 1000
         initial_asa_position_algo = price * asa_position / 10 ** 6
 
         self.logger.info(f'Initial ASA position in Algo units: {initial_asa_position_algo}')
 
-        impact_timescale_seconds = 60 * 60
+        impact_timescale_seconds = 5 * 60
 
-        simulation_step_seconds = 5
+        simulation_step_seconds = 5 * 60
         simulation_step = datetime.timedelta(seconds=simulation_step_seconds)
 
         algo_reserves_decimal = mualgo_reserves / (10 ** 6)
@@ -41,54 +41,78 @@ class TestOptimizer(unittest.TestCase):
         t = datetime.datetime.utcnow()
         impact_state = ASAImpactState(decay_timescale_seconds=impact_timescale_seconds, t=t)
 
-        optimiser = Optimizer(asset1=asa_asset_id, risk_bps_per_algo=risk_bps_per_algo)
+        optimiser = Optimizer(asset1=asa_asset_id, risk_coef=risk_coef)
 
-        impact_values = []
-        positions = []
+        # Simulate n days
+        n_days = 2
 
-        n_days = 10
+        steps = list(range((n_days * 24 * 60 * 60) // simulation_step_seconds))
 
-        lost_fees = 0
+        lost_fees = np.zeros(len(steps))
+        linear_impact_costs = np.zeros(len(steps))
+        quadratic_impact_costs = np.zeros(len(steps))
 
-        # Simulate 5 days
-        for i in range((n_days * 24 * 60 * 60) // simulation_step_seconds):
+        impact_values = np.zeros(len(steps))
+        positions = np.zeros(len(steps))
+        algo_positions = np.zeros(len(steps))
+
+        times = []
+
+        for i in steps:
+
+            times.append(t)
 
             impact_bps = impact_state.value(t)
-            swap = optimiser.optimal_amount_swap(signal_bps=0,
-                                                 impact_bps=impact_bps,
-                                                 current_asa_position=asa_position,
-                                                 current_asa_reserves=asa_reserves,
-                                                 current_mualgo_reserves=mualgo_reserves
-                                                 )
-            if swap is not None:
-                assert swap.asset_buy == AssetType.ALGO
+            swap_info = optimiser.optimal_amount_swap(signal_bps=0,
+                                                      impact_bps=impact_bps,
+                                                      current_asa_position=asa_position,
+                                                      current_asa_reserves=asa_reserves,
+                                                      current_mualgo_reserves=mualgo_reserves
+                                                      )
+            impact_values[i] = impact_state.value(t)
+            positions[i] = asa_position * price / 10 ** 6
+            algo_positions[i] = mualgo_position / 10 ** 6
 
-                amount_buy = swap.optimised_buy.amount
-                amount_sell = int(swap.optimised_buy.amount * price)
+            if swap_info is not None:
+                assert swap_info.optimal_swap.asset_buy == AssetType.ALGO
+
+                amount_buy = swap_info.optimal_swap.optimised_buy.amount
+                amount_sell = int(swap_info.optimal_swap.optimised_buy.amount / price)
 
                 asa_position -= amount_sell
-                algo_position += amount_buy
+                mualgo_position += amount_buy
 
                 traded_swap = AlgoPoolSwap(asset_buy=0, amount_buy=amount_buy, amount_sell=amount_sell)
                 impact_state.update(traded_swap, mualgo_reserves, asa_reserves, t)
 
-                asa_position_algo = asa_position * price / 10 ** 6
-                impact_state_value = impact_state.value(t)
+                lost_fees[i] = FIXED_FEE_ALGOS
+                linear_impact_costs[i] = swap_info.trade_costs_mualgo.linear_impact_cost_mulago / 10 ** 6
+                quadratic_impact_costs[i] = swap_info.trade_costs_mualgo.quadratic_impact_cost_mulago / 10 ** 6
 
-                impact_values.append(impact_state_value)
-                positions.append(asa_position_algo)
-                lost_fees += FIXED_FEE_ALGOS
-
-                self.logger.info(f'{t}: Sold ASA algo amount {traded_swap.amount_sell * price / 10 ** 6}. '
-                                 f'ASA algo position = {asa_position_algo}, '
-                                 f'Impact state = {impact_state_value}')
+                self.logger.info(f'{t}: Sold ASA algo amount {traded_swap.amount_sell * price / 10 ** 6}.')
 
             t += simulation_step
 
-        f, axs = plt.subplots(1, 2, figsize=(10, 5))
-        axs[0].plot(positions)
-        axs[0].set_title('Position')
-        axs[1].plot(impact_values)
-        axs[1].set_title('Impact')
-        f.suptitle(f'Lost fees = {lost_fees}')
+        self.logger.info(f'Final Algo position: {mualgo_position / 10 ** 6}.')
+
+        f, axs = plt.subplots(2, 2, figsize=(10, 12), sharex='col')
+
+        axs[0][0].plot(times, positions, label='ASA position (algo basis)')
+        axs[0][0].plot(times, algo_positions, label='Algo position')
+        axs[0][0].legend()
+        axs[0][0].set_title('Positions')
+        axs[0][1].plot(times,impact_values)
+        axs[0][1].set_title('Impact')
+
+        axs[1][0].plot(times, lost_fees.cumsum(), label='lost_fees')
+        axs[1][0].plot(times, linear_impact_costs.cumsum(), label='linear_impact_costs')
+        axs[1][0].plot(times, quadratic_impact_costs.cumsum(), label='quadratic_impact_costs')
+        axs[1][0].set_title('Accrued costs')
+
+        axs[1][0].legend()
+        # f.tight_layout()
+        axs[1][0].tick_params(labelrotation=45)
+
+        axs[1][1].tick_params(labelrotation=45)
+
         plt.show()
